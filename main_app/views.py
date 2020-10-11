938855526e9e6bb2e544cb45b0777e7943c9e5be
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-10-11 15:34:48 -0400

    read primitive db

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Piece, pieces
 
 def home(request):
   return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
@@ -8,4 +9,4 @@ def about(request):
     return render(request, 'about.html')
 
 def pieces_index(request):
-    return render(request, 'pieces/index.html', {'pieces': pieces})
+    return render(request, 'pieces/index.html', { 'pieces': pieces })   
