ec26147f306c4eb527ea41c58d9d2db3dc350b64
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-10-11 15:08:16 -0400

    add piece model

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
+class Piece:
+    def __init__(self, 'name', 'composer', 'period', 'instrument', 'voice', 'own'):
+        self.name = name
+        self.composer = composer
+        self.period = period
+        self.instrument = instrument
+        self.voice = voice
+        self.own = own
+
+pieces = [
+    Piece('I know that my redeemer Liveth', 'GF Handel', 'Baroque', 'Piano', 'Soprano', True),
+    Piece('Come Unto Him', 'GF Handel', 'Baroque', 'Piano', 'Soprano', True),
+    Piece('Fugue No. 2', 'JS Bach', 'Baroque', 'Piano', 'n/a', True)
+]
