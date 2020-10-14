954b39586efe998729ea52cfd58e475ede897f6f
aaalexandriaaa <67809768+aaalexandriaaa@users.noreply.github.com>
2020-10-14 16:52:39 -0400

    comment out some hopefully extraneous code

--- sforzando/settings.py
+++ sforzando/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import environ, os, django_heroku
+import environ,
+# os, django_heroku
 
 env = environ.Env(
     # set casting, default value
@@ -23,15 +24,15 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+            # # Static files (CSS, JavaScript, Images)
+            # # https://docs.djangoproject.com/en/1.9/howto/static-files/
+            # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+            # STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+            # # Extra places for collectstatic to find static files.
+            # STATICFILES_DIRS = (
+            #     os.path.join(BASE_DIR, 'static'),
+            # )
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
